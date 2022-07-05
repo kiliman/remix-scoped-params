208fbba3c6f3cb0aebfd7a8bbac5144e781a4a39
kiliman <kiliman@gmail.com>
2022-07-05 18:37:36 -0400

    🔨 Set default scope to empty

--- app/utils/scopedParams.ts
+++ app/utils/scopedParams.ts
@@ -1,29 +1,33 @@
 import { useLocation } from '@remix-run/react'
 
-export function getScopedParams(request: Request, scope: string) {
+export function getScopedParams(request: Request, scope: string = '') {
   const url = new URL(request.url)
-  return new URLSearchParams(
-    Array.from(url.searchParams.entries())
-      .filter(([key]) => key.startsWith(`${scope}:`))
-      .map(([key, value]) => [key.replace(`${scope}:`, ''), value]),
-  )
+  // if scope is not provided, return current searchParams
+  return scope === ''
+    ? url.searchParams
+    : new URLSearchParams(
+        Array.from(url.searchParams.entries())
+          .filter(([key]) => key.startsWith(`${scope}:`))
+          .map(([key, value]) => [key.replace(`${scope}:`, ''), value]),
+      )
 }
 
 type ParamsType = Record<string, string | number | boolean | undefined>
-export function useScopedParams(scope: string) {
+export function useScopedParams(scope: string = '') {
   const location = useLocation()
   const searchParams = new URLSearchParams(location.search)
+  const prefix = scope ? `${scope}:` : ''
   const updateSearchParams = (params: ParamsType) => {
     Object.entries(params).forEach(([key, value]) => {
       if (value === undefined) {
-        searchParams.delete(`${scope}:${key}`)
+        searchParams.delete(`${prefix}${key}`)
       } else {
-        searchParams.set(`${scope}:${key}`, String(value))
+        searchParams.set(`${prefix}${key}`, String(value))
       }
     })
   }
   return {
-    getParam: (key: string) => searchParams.get(`${scope}:${key}`),
+    getParam: (key: string) => searchParams.get(`${prefix}${key}`),
     getScopedSearch: (params: ParamsType) => {
       updateSearchParams(params)
       let search = searchParams.toString()
